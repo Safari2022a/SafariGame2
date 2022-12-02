9b3213aa67d2769498fbf8f1a0d8079a4e458f48
nyantama0616 <r_nyantama0616@yahoo.co.jp>
2022-12-02 22:07:38 +0900

    頭なでた、お尻撫でたっていう判定の処理できた.。

--- Assets/Scripts/Animals/AnimalBase.cs
+++ Assets/Scripts/Animals/AnimalBase.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+public enum AnimalState {
+    Normal,
+    Happy,
+    Hate,
+}
+
 public class AnimalBase : MonoBehaviour, IPointerClickHandler
 {
     string _nickName = "ダマジカ";
     public string NickName { get { return _nickName; } }
 
-    bool isHeart = false;
-    bool isHate = false;
+    [SerializeField] GameObject happyEffectPrefab;
+    [SerializeField] GameObject hateEffectPrefab;
+
+    GameObject happyEffect;
+    GameObject hateEffect;
+
+    AnimalState state = AnimalState.Normal;
+
+    string happyStr = "qweasdzxc";
+    string hateStr = "yuihjknm";
+
+    // bool isHeart = false;
+    // bool isHate = false;
 
     void Start() {
         GameObject heart = transform.Find("Effects/heart/Particle System").gameObject;
@@ -24,30 +41,62 @@ public class AnimalBase : MonoBehaviour, IPointerClickHandler
         if (pc.CurrentPanel == Panel.AnimalSelect) {
             pc.OnAnimalClick(gameObject);
         }
-
-        if (isHeart) {
-            GameObject heart = transform.Find("Effects/heart/Particle System").gameObject;
-            heart.transform.localScale = new Vector3(0.11613f, 0.11613f, 0.11613f);
-        } else if (isHate) {
-            GameObject hate = transform.Find("Effects/hate/Particle System").gameObject;
-            hate.transform.localScale = new Vector3(0.11613f, 0.11613f, 0.11613f);
-        }
     }
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.O)) {
-            isHeart = true;
-            isHate = false;
-        } else if (Input.GetKeyDown(KeyCode.P)) {
-            isHeart = false;
-            isHate = true;
-        } else if (Input.GetKeyDown(KeyCode.L)) {
-            isHeart = false;
-            isHate = false;
-            GameObject heart = transform.Find("Effects/heart/Particle System").gameObject;
-            heart.transform.localScale = Vector3.zero;
-            GameObject hate = transform.Find("Effects/hate/Particle System").gameObject;
-            hate.transform.localScale = Vector3.zero;
+        for (int i = 0; i < happyStr.Length; i++) {
+            if (Input.GetKeyDown(happyStr[i].ToString())) {
+                StartHappy();
+                StartCoroutine(Utility.DelayCoroutine(3, EndHappy));
+            }
         }
+        
+        for (int i = 0; i < hateStr.Length; i++) {
+            if (Input.GetKeyDown(hateStr[i].ToString())) {
+                StartHate();
+                StartCoroutine(Utility.DelayCoroutine(3, EndHate));
+            }
+        }
+    }
+
+    void StartHappy() {
+        if (state == AnimalState.Happy) return;
+        if (state == AnimalState.Hate) {
+            EndHate();
+        }
+
+        print("Happy");
+        happyEffect = Instantiate(happyEffectPrefab);
+        happyEffect.transform.SetParent(transform.Find("Effects"));
+        happyEffect.transform.localPosition = Vector3.zero;
+
+        state = AnimalState.Happy;
+    }
+
+    void EndHappy() {
+        if (state != AnimalState.Happy) return;
+
+        Destroy(happyEffect);
+        state = AnimalState.Normal;
+    }
+    
+    void StartHate() {
+        if (state == AnimalState.Hate) return;
+        if (state == AnimalState.Happy) {
+            EndHappy();
+        }
+
+        hateEffect = Instantiate(hateEffectPrefab);
+        hateEffect.transform.SetParent(transform.Find("Effects"));
+        hateEffect.transform.localPosition = Vector3.zero;
+        
+        state = AnimalState.Hate;
+    }
+    
+    void EndHate() {
+        if (state != AnimalState.Hate) return;
+
+        Destroy(hateEffect);
+        state = AnimalState.Normal;
     }
 }
